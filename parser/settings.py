abf797893b4216dc252e67975639758e3468a8c3
Ian A Wilson <ian.a.wilson@rochester.edu>
2011-12-05 22:26:40 -0500

    Changed django api application to be called parser. Lots of updates to README.

--- parser/settings.py
+++ parser/settings.py
@@ -1,4 +1,4 @@
-# Django settings for api project.
+# Django settings for parser project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'api.urls'
+ROOT_URLCONF = 'parser.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
