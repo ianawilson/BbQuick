0229430f6b6cb8816f2cdc390ac0ccd941226d2c
Chelsea <cflint@u.rochester.edu>
2011-12-08 00:11:09 -0500

    updated main page population

--- chrome/bbq.js
+++ chrome/bbq.js
@@ -1,6 +1,18 @@
 $(document).ready(init);
 
+var button = "<div class='button'></div>";
+
 function init() {
+
+	for (i = 0; i<data["courses"].length; i++) {
+		var div = $(button).html(data["courses"][i]["name"]);
+
+		div.click(function() {
+			showCourse(i);
+		});
+	
+		$("#courses").append(div);
+	}
 	
 	$(".button").mouseover(function() {
 		$(this).addClass("mouseover");
@@ -8,4 +20,9 @@ function init() {
 	$(".button").mouseout(function() {
 		$(this).removeClass("mouseover");
 	});
+}
+
+function showCourse(buttonID) {
+	$(".wrapper").hide();
+	$("#class").show();
 }
--- chrome/populate.js
+++ chrome/populate.js
@@ -5,23 +5,23 @@ var courseArr = new Array();
 
 var course1 = new Array();
 course1["name"] = "Human Computer Interaction";
-course1["url"] = "javascript:show(class1);";
-course1["shortName"] = "csc212";
+course1["url"] = "my.rochester.edu";
+course1["shortname"] = "csc212";
 
 var course2 = new Array();
 course2["name"] = "Pervasive Computing";
-course2["url"] = "javascript:show(class2);";
-course2["shortName"] = "csc297";
+course2["url"] = "my.rochester.edu";
+course2["shortname"] = "csc297";
 
 var course3 = new Array();
 course3["name"] = "Film History: Early Cinema";
-course3["url"] = "javascript:show(class3);";
-course3["shortName"] = "eng255";
+course3["url"] = "my.rochester.edu";
+course3["shortname"] = "eng255";
 
 var course4 = new Array();
 course4["name"] = "The Poetics of Television";
-course4["url"] = "javascript:show(class4);";
-course4["shortName"] = "eng263";
+course4["url"] = "my.rochester.edu";
+course4["shortname"] = "eng263";
 
 courseArr[0] = course1;
 courseArr[1] = course2;
@@ -32,16 +32,3 @@ data["courses"] = courseArr;
 
 //alert(data["courses"][3]["name"]);
 /*end dummy data setup*/
-
-$(document).ready(function() {
-	for (i = 0; i<data["courses"].length; i++) {
-		$("#courses").append("<a href='" +
-					data["courses"][i]["url"] + "'><div class='button'>" +
-					data["courses"][i]["name"] + "</div></a>");
-	}
-});
-
-function show(classID) {
-	$(".wrapper").addClass("hidden");
-	$(classID).removeClass("hidden");
-}
